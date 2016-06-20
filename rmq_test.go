dbf4fe73cde4406cc76d82174a4b722fec3f24cb
Yeshwanth Kumar <morpheyesh@gmail.com>
2016-06-20 17:58:59 +0530

    tests

--- rmq_test.go
+++ rmq_test.go
@@ -1,1 +1,24 @@
 package amqp
+
+import (
+	"testing"
+  "fmt"
+	"gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type S struct{}
+
+var _ = check.Suite(&S{})
+
+func (s *S) Testdial(c *check.C) {
+	f, err := Init()
+	c.Assert(err, check.IsNil)
+  qu, ok := f.New("test-queue")
+  body := []byte("test-data")
+  qu.Publish(body)
+	c.Assert(ok, check.Equals, true)
+}
